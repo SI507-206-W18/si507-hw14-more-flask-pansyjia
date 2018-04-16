316dcfdf36748c64df2a4ea5cc7258bbf3e31244
pansyjia <siyujia@umich.edu>
2018-04-16 15:20:31 -0400

    first commit

--- app.py
+++ app.py
@@ -20,6 +20,26 @@ def postentry():
     model.add_entry(name, message)
     return redirect("/")
 
+
+@app.route("/admin")
+def admin():
+    print(123)
+    return(render_template("admin.html", entries=model.get_entries()))
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    print('delete action')
+    idnum = request.form["aaa"]
+    print(request.form["aaa"])
+    # m = request.form["bbb"]
+    print(request.form["bbb"])
+    print("id", idnum)
+    model.delete_entry(idnum)
+
+    return redirect('/admin')
+
+    
+
 if __name__=="__main__":
     model.init()
     app.run(debug=True)
--- model.py
+++ model.py
@@ -33,3 +33,24 @@ def add_entry(name, text):
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
+
+
+def delete_entry(clicked_id):
+    #check every element in the list to see if that element is the element you are going to delete
+    # have a for loop to check the id in the list to see if the element is the element you are going to delete
+    global entries, GUESTBOOK_ENTRIES_FILE
+    for ele in entries:
+        if int(clicked_id) == list(ele.values())[-1]:
+            try:
+                entries.remove(ele)
+            except:
+                pass
+    try:
+        f = open(GUESTBOOK_ENTRIES_FILE, "w")
+        empty_st = json.dumps(entries)
+        f.write(empty_st)
+        f.close()
+    except:
+        print("Please try again! Error" )
+
+    print(entries)
